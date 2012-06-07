e219c99c4fdf047a5281cd3fb35363773a4fd373
fakestarbaby <fakestarbaby@gmail.com>
2012-06-07 09:52:55 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRocketPants::Application.config.secret_token = 'd19ac8aebad90a0b4a26b6647dad53a8cd63e445d59f44532a185cf6ef6671bf5e72a71ec7a79c1794478f57044490a86c53493f01e1e781d0d1cc55bd4e74d7'
